98d2ad32b22cf79e8ee0a93e92d745079d0361b4
man572142 <87473853+man572142@users.noreply.github.com>
2022-10-30 18:45:27 +0800

    add button sound trigger

---
+++ Assets/Scripts/Audio/ButtonSoundTrigger.cs
@@ -0,0 +1,33 @@
+using MiProduction.BroAudio;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonSoundTrigger : MonoBehaviour
+{
+    [SerializeField] Button _button = null;
+    [SerializeField] UI _uiSound = UI.None;
+
+    private void Start()
+    {
+        if(_button == null)
+        {
+            _button = GetComponent<Button>();
+        }
+
+        _button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnDestroy()
+    {
+        _button.onClick.RemoveListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        SoundSystem.PlaySFX((int)UI.ButtonClick);
+    }
+}

28f8e5a80c10911cdaf26d77bf9e71c08173889d
man572142 <man572142@gmail.com>
2023-03-06 14:12:10 +0800

    Function: add SetVolume with IAudioPlayer

--- Assets/MiProduction/BroAudio/Scripts/Interface/IAudioPlayer.cs
+++ Assets/MiProduction/BroAudio/Scripts/Interface/IAudioPlayer.cs
@@ -1,6 +1,10 @@
 public interface IAudioPlayer
 {
+    public IAudioPlayer SetVolume(float vol, float fadeTime = 1f);
+
     public IAudioPlayer StandsOut(float standoutRatio, float fadeTime);
 
     public IAudioPlayer LowPassOthers(float freq, float fadeTime);
+
+
 }

95c588edec9f295f5b6e3843df9c868620a0510a
man572142 <man572142@gmail.com>
2023-03-31 18:26:12 +0800

    Refactoring audio clip editing function

--- Assets/MiProduction/Extension/AudioExtension.cs
+++ Assets/MiProduction/Extension/AudioExtension.cs
@@ -7,6 +7,32 @@ namespace MiProduction.Extension
 {
     public static class AudioExtension
     {
+        public struct AudioClipSetting
+		{
+            public readonly float Length;
+            public readonly int Frequency;
+            public readonly int Channels;
+            public readonly int Samples;
+            public readonly bool Ambisonic;
+            public readonly AudioClipLoadType LoadType;
+            public readonly bool PreloadAudioData;
+            public readonly bool LoadInBackground;
+            public readonly AudioDataLoadState LoadState;
+
+			public AudioClipSetting(AudioClip originClip)
+			{
+				Length = originClip.length;
+				Frequency = originClip.frequency;
+				Channels = originClip.channels;
+				Samples = originClip.samples;
+				Ambisonic = originClip.ambisonic;
+				LoadType = originClip.loadType;
+				PreloadAudioData = originClip.preloadAudioData;
+				LoadInBackground = originClip.loadInBackground;
+				LoadState = originClip.loadState;
+			}
+		}
+
         public const float HaasEffectInSecond = 0.04f;
 
         public const float MinDecibelVolume = -80f;
@@ -39,60 +65,86 @@ namespace MiProduction.Extension
             return Mathf.Clamp(dB,MinDecibelVolume, MaxDecibelVolume);
         }
 
-        public static AudioClip Trim(this AudioClip originClip, float startPos, float endPos,string newClipName)
+        public static bool TryGetSampleData(this AudioClip originClip,out float[] sampleArray, float startPosInSecond = 0f, float endPosInSecond = 0f)
         {
-            int startSample = (int)(startPos * originClip.frequency);
-            int sampleLength = (int)((originClip.length - endPos - startPos) * originClip.frequency);
-            float[] sampleArray = new float[sampleLength];
+            int startSample = (int)(startPosInSecond * originClip.samples * originClip.channels);
+            int sampleLength = (int)((originClip.length - endPosInSecond - startPosInSecond) * originClip.frequency * originClip.channels);
+            
+            sampleArray = new float[sampleLength];
             bool sucess = originClip.GetData(sampleArray, startSample);
 
+            if(!sucess)
+			{
+                Debug.LogError($"Can't get audio clip : {originClip.name} 's sample data!");
+			}
+            return sucess;
+        }
+
+        public static AudioClip CreateAudioClip(string name,float[] samples,AudioClipSetting setting)
+		{
+            AudioClip result = AudioClip.Create(name, samples.Length, setting.Channels, setting.Frequency, setting.LoadType == AudioClipLoadType.Streaming);
+            result.SetData(samples, 0);
+            return result;
+        }
+
+        public static AudioClipSetting GetAudioClipSetting(this AudioClip audioClip)
+		{
+            return new AudioClipSetting(audioClip);
+		}
+
+        public static AudioClip Trim(this AudioClip originClip, float startPos, float endPos,string newClipName)
+        {
             AudioClip resultClip = null;
-            if (sucess)
-            {
-                resultClip = AudioClip.Create(newClipName, sampleLength, originClip.channels, originClip.frequency, originClip.loadType == AudioClipLoadType.Streaming);
-                resultClip.SetData(sampleArray, 0);
+            if (originClip.TryGetSampleData(out var sampleArray, startPos, endPos))
+			{
+                AudioClipSetting settings = originClip.GetAudioClipSetting();
+                resultClip = CreateAudioClip(newClipName, sampleArray, settings);
             }
 
             return resultClip;
         }
 
-        public static AudioClip Boost(this AudioClip originClip, float boostVol, string clipNameSuffix = "_Edited")
+        public static AudioClip Boost(this AudioClip originClip, float boostVolInDb, string newClipName)
 		{
-            float[] sampleArray = new float[originClip.samples];
-            originClip.GetData(sampleArray, 0);
-
-            for(int i = 0; i < sampleArray.Length;i++)
+			if(!originClip.TryGetSampleData(out var sampleArray))
 			{
-                int sign = 1;
-                float vol = sampleArray[i];
-                if(vol < 0)
-				{
-                    sign = -1;
-                    vol *= sign;
-				}
-
-                float db = vol.ToDecibel();
-                db += boostVol;
-
-                sampleArray[i] = db.ToNormalizeVolume() * sign;
+                return null;
 			}
 
-            AudioClip resultClip = AudioClip.Create(originClip.name + clipNameSuffix, sampleArray.Length, originClip.channels, originClip.frequency, originClip.loadType == AudioClipLoadType.Streaming);
-            resultClip.SetData(sampleArray, 0);
-            return resultClip;
-		}
+			for (int i = 0; i < sampleArray.Length; i++)
+			{
+				int sign = 1;
+				float vol = sampleArray[i];
+				if (vol < 0)
+				{
+					sign = -1;
+					vol *= sign;
+				}
 
-        public static AudioClip Reverse(this AudioClip originClip,string clipNameSuffix = "_Edited")
+				float db = vol.ToDecibel();
+				db += boostVolInDb;
+
+				sampleArray[i] = db.ToNormalizeVolume() * sign;
+			}
+            AudioClipSetting setting = originClip.GetAudioClipSetting();
+			return CreateAudioClip(newClipName, sampleArray, setting);
+        }
+
+		
+
+		public static AudioClip Reverse(this AudioClip originClip,string newClipName)
 		{
-            float[] sampleArray = new float[originClip.samples];
-            originClip.GetData(sampleArray, 0);
+            if (!originClip.TryGetSampleData(out var sampleArray))
+            {
+                return null;
+            }
 
             Array.Reverse(sampleArray);
 
-            AudioClip resultClip = AudioClip.Create(originClip.name + clipNameSuffix, sampleArray.Length, originClip.channels, originClip.frequency, originClip.loadType == AudioClipLoadType.Streaming);
-            resultClip.SetData(sampleArray, 0);
-            return resultClip;
+            AudioClipSetting setting = originClip.GetAudioClipSetting();
+            return CreateAudioClip(newClipName, sampleArray, setting);
         }
+
     }
 
 }

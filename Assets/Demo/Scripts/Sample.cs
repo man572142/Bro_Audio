fb5cc09792fda6d9fe5dac65fc4178d6ae3cf77a
man572142 <man572142@gmail.com>
2023-09-22 17:10:26 +0800

    Add README pictures

--- Assets/Demo/Scripts/Sample.cs
+++ Assets/Demo/Scripts/Sample.cs
@@ -19,17 +19,7 @@ public class Sample : MonoBehaviour
 
 	void Start()
     {
-		BroAudio.Play(_musicA);
-		StartCoroutine(Test());
-		
-    }
-
-	private IEnumerator Test()
-	{
-		yield return new WaitForSeconds(3f);
-        BroAudio.Play(_musicB).AsDominator().QuietOthers(0.7f).LowPassOthers(1000f);
-		yield return new WaitForSeconds(3f);
-        BroAudio.Play(_voiceOver).AsDominator().QuietOthers(0.1f);
+		BroAudio.Play(_musicA);	
     }
 
 

bc36e0aa433fb2051ddf88c951536d5f418699c4
man572142 <man572142@gmail.com>
2024-02-01 16:38:19 +0800

    Delete easy trigger feature

--- Assets/BroAudio/Scripts/MonoComponent/SoundSource.cs
+++ Assets/BroAudio/Scripts/MonoComponent/SoundSource.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Ami.Extension;
 
 namespace Ami.BroAudio
 {
     [AddComponentMenu("BroAudio/" + nameof(SoundSource))]
     public class SoundSource : MonoBehaviour
     {
-        [SerializeField] AudioID _sound = default;
+        public enum PositionMode
+        {
+            Global,
+            FollowGameObject,
+            StayHere,
+        }
+
+		[SerializeField] bool _playOnStart = true;
+		[SerializeField] AudioID _sound = default;
+        [SerializeField] PositionMode _positionMode = default;
 
         public void Play() => BroAudio.Play(_sound);
         public void Play(Transform followTarget) => BroAudio.Play(_sound, followTarget);
         public void Play(Vector3 positon) => BroAudio.Play(_sound, positon);
+        public void Stop() => BroAudio.Stop(_sound);
+		public void Stop(float fadeTime) => BroAudio.Stop(_sound,fadeTime);
+        public void SetVolume(float vol) => BroAudio.SetVolume(_sound,vol);
 
-    }
+
+		private void Start()
+		{
+            if(!_playOnStart)
+            {
+                return;
+            }
+
+            switch(_positionMode)
+            {
+                case PositionMode.Global:
+                    Play();
+                    break;
+                case PositionMode.FollowGameObject:
+                    Play(transform);
+                    break;
+                case PositionMode.StayHere:
+                    Play(transform.position);
+                    break;
+            }
+		}
+	}
 }

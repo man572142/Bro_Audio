2383c285555333c5f0155949783568e3e64d9e10
man572142 <man572142@gmail.com>
2024-04-01 11:50:05 +0800

    Add minimum letterbox height to improve demo compatibility

--- Assets/BroAudio/Demo/Scripts/UI/LetterBoxSetter.cs
+++ Assets/BroAudio/Demo/Scripts/UI/LetterBoxSetter.cs
@@ -13,25 +13,22 @@ namespace Ami.BroAudio.Demo
 		[SerializeField] RectTransform _letterBoxTop = null;
 		[SerializeField] RectTransform _letterBoxBottom = null;
 		[SerializeField] float _targetRatio = default;
-		private float _barHeight = 0f;
+		[SerializeField] float _minimumBoxHeight = 70f;
+		private float _boxHeight = 0f;
 
 		private void Start()
 		{
 			float screenWidth = _canvasRectTransform.rect.width;
 			float screenHeight = _canvasRectTransform.rect.height;
-			if (_targetRatio <= screenWidth / screenHeight)
-			{
-				Debug.LogError("Letterbox's target ratio should be greater than the current screen aspect ratio");
-				return;
-			}
 
-            _barHeight = (screenHeight - screenWidth / _targetRatio) * 0.5f;
+			_boxHeight = (screenHeight - screenWidth / _targetRatio) * 0.5f;
+			_boxHeight = Mathf.Max(_minimumBoxHeight, _boxHeight);
 
-			_letterBoxTop.sizeDelta = new Vector2(screenWidth, _barHeight);
-			_letterBoxTop.anchoredPosition = new Vector2(0f, _barHeight);
+			_letterBoxTop.sizeDelta = new Vector2(screenWidth, _boxHeight);
+			_letterBoxTop.anchoredPosition = new Vector2(0f, _boxHeight);
 
-			_letterBoxBottom.sizeDelta = new Vector2(screenWidth, _barHeight);
-			_letterBoxBottom.anchoredPosition = new Vector2(0f, -_barHeight);
+			_letterBoxBottom.sizeDelta = new Vector2(screenWidth, _boxHeight);
+			_letterBoxBottom.anchoredPosition = new Vector2(0f, -_boxHeight);
 
 		}
 
@@ -64,11 +61,11 @@ namespace Ami.BroAudio.Demo
 		//	{
 		//		_letterBoxTop.anchoredPosition = new Vector2(0f, Mathf.Lerp(_barHeight, 0f, time / duration));
 		//		_letterBoxBottom.anchoredPosition = new Vector2(0f, Mathf.Lerp(-_barHeight, 0f, time / duration));
-	
+
 		//		yield return null;
 		//		time += isIn? Time.deltaTime : -Time.deltaTime;
 		//	}
 		//	OnRollFinished?.Invoke();
 		//}
-	} 
+	}
 }

ab7abc14f7cb4345223fbcd6ded5ac0bb2a9e3cf
man572142 <man572142@gmail.com>
2024-04-12 21:10:24 +0800

    Skip warning in demo note

--- Assets/BroAudio/Demo/Scripts/AudioReverbZoneNote.cs
+++ Assets/BroAudio/Demo/Scripts/AudioReverbZoneNote.cs
@@ -6,7 +6,9 @@ namespace Ami.BroAudio.Demo
 {
     public class AudioReverbZoneNote : MonoBehaviour
     {
+#pragma warning disable 414
         [SerializeField] GameObject _addtionalNote = null;
+#pragma warning restore 414
         void Start()
         {
 #if UNITY_2021

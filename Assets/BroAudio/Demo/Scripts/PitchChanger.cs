ce9d74d7a94bd317d2961e438757e4f6d889d597
man572142 <man572142@gmail.com>
2024-06-29 23:29:53 +0800

    Add pitch feature to demo scene
    update

---
+++ Assets/BroAudio/Demo/Scripts/PitchChanger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ami.BroAudio.Demo
+{
+	public class PitchChanger : MonoBehaviour
+	{
+		[SerializeField, Pitch] float _pitch = 1f;
+		[SerializeField] BroAudioType _targetAudioType = BroAudioType.All;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                BroAudio.SetPitch(_pitch, _targetAudioType);
+            }
+        }
+    }
+}

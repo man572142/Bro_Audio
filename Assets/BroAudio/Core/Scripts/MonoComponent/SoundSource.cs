a4c5fac1f151333dbfe6272fcaa4cceebba19e03
man572142 <man572142@gmail.com>
2024-07-14 15:13:48 +0800

    Update soundSource with replay mechanic

--- Assets/BroAudio/Core/Scripts/MonoComponent/SoundSource.cs
+++ Assets/BroAudio/Core/Scripts/MonoComponent/SoundSource.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Ami.BroAudio
 {
@@ -14,15 +13,18 @@ namespace Ami.BroAudio
             StayHere,
         }
 
-		[SerializeField] bool _playOnStart = true;
-		[SerializeField] SoundID _sound = default;
+        [SerializeField] bool _onlyPlayOnce = false;
+        [SerializeField, FormerlySerializedAs("_playOnStart")] bool _playOnEnable = true;
+        [SerializeField] bool _stopOnDisable = false;
+        [Space]
+        [SerializeField] SoundID _sound = default;
         [SerializeField] PositionMode _positionMode = default;
 
         public void Play() => _currentPlayer = BroAudio.Play(_sound);
         public void Play(Transform followTarget) => _currentPlayer = BroAudio.Play(_sound, followTarget);
         public void Play(Vector3 positon) => _currentPlayer = BroAudio.Play(_sound, positon);
-        public void Stop() =>  _currentPlayer?.Stop();
-		public void Stop(float fadeTime) => _currentPlayer?.Stop(fadeTime);
+        public void Stop() => _currentPlayer?.Stop();
+        public void Stop(float fadeTime) => _currentPlayer?.Stop(fadeTime);
         public void SetVolume(float vol) => _currentPlayer?.SetVolume(vol);
         public void SetVolume(float vol, float fadeTime) => _currentPlayer?.SetVolume(vol, fadeTime);
         public void SetPitch(float pitch) => _currentPlayer?.SetPitch(pitch);
@@ -30,15 +32,14 @@ namespace Ami.BroAudio
 
         private IAudioPlayer _currentPlayer;
 
-
-		private void Start()
-		{
-            if(!_playOnStart)
+        private void OnEnable()
+        {
+            if (!_playOnEnable)
             {
                 return;
             }
 
-            switch(_positionMode)
+            switch (_positionMode)
             {
                 case PositionMode.Global:
                     Play();
@@ -50,6 +51,19 @@ namespace Ami.BroAudio
                     Play(transform.position);
                     break;
             }
-		}
-	}
+
+            if (_onlyPlayOnce)
+            {
+                _playOnEnable = false;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if(_stopOnDisable && _currentPlayer != null && _currentPlayer.IsPlaying)
+            {
+                _currentPlayer.Stop();
+            }
+        }
+    }
 }

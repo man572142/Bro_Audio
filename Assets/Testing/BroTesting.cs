a1fb1067b9d02d8418193a0eb5247b326f69184d
man572142 <man572142@gmail.com>
2024-07-16 18:45:28 +0800

    Add ultimate testing scene
    update testing scene
    
    Update testing scene

---
+++ Assets/Testing/BroTesting.cs
@@ -0,0 +1,52 @@
+using Ami.Extension;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ami.BroAudio.Testing
+{
+    public class BroTesting : MonoBehaviour
+    {
+        [SerializeField] SoundID _sound = default;
+
+        [SerializeField] private Button _btn;
+
+        [Space]
+        [SerializeField] float _fadeTime = 0f;
+        [SerializeField] BroAudioType _audioType = default;
+        [SerializeField, Volume] float _volume = 1f;
+        [SerializeField, Pitch] float _pitch = 1f;
+        [SerializeField, Frequency] float _frequency = AudioConstant.MinFrequency;
+        [SerializeField] float _customEffectValue = 0f;
+        [SerializeField] Transition _transition = default;
+        [SerializeField] Fading _fading = default;
+
+        private IAudioPlayer _player;
+
+        public void Play() => _player = BroAudio.Play(_sound);
+
+        public void SetVolume() => BroAudio.SetVolume(_volume, _fadeTime);
+        public void SetVolume(BroAudioType audioType, int i) {}
+        public void SetAudioTypeVolume() => BroAudio.SetVolume(_audioType, _volume, _fadeTime);
+        public void SetSoundIDVolume() => BroAudio.SetVolume(_sound, _volume, _fadeTime);
+        public void PlayerSetVolume() => _player.SetVolume(_volume, _fadeTime);
+
+        public void SetPitch() => BroAudio.SetPitch(_pitch, _fadeTime);
+        public void SetAudioTypePitch() => BroAudio.SetPitch(_pitch, _audioType, _fadeTime);
+        public void PlayerSetPitch() => _player.SetPitch(_pitch, _fadeTime);
+
+        public void PlayerStop() => _player.Stop(_fadeTime);
+        public void StopAudioType() => BroAudio.Stop(_audioType, _fadeTime);
+        public void StopSoundID() => BroAudio.Stop(_sound, _fadeTime);
+
+        public void SetLowPassFilter() => BroAudio.SetEffect(Effect.LowPass(_frequency, _fadeTime), _audioType);
+        public void ResetLowPassFilter() => BroAudio.SetEffect(Effect.ResetLowPass(_fadeTime), _audioType);
+        public void SetHighPassFilter() => BroAudio.SetEffect(Effect.HighPass(_frequency, _fadeTime), _audioType);
+        public void ResetHighPassFilter() => BroAudio.SetEffect(Effect.ResetHighPass(_fadeTime), _audioType);
+        public void SetCustomEffect(string paraName) => BroAudio.SetEffect(Effect.Custom(paraName, _customEffectValue), _audioType);
+
+        public void PlayerAsLowPassDominator() => _player.AsDominator().LowPassOthers(_frequency, _fading);
+        public void PlayerAsHighPassDominator() => _player.AsDominator().HighPassOthers(_frequency, _fading);
+
+        public void PlayerAsBGM() => _player.AsBGM().SetTransition(_transition, _fadeTime);
+    }
+}
---
+++ Assets/Testing/BroTestingButton.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ami.Extension;
+using Ami.BroAudio.Runtime;
+
+namespace Ami.BroAudio.Testing
+{
+    [DisallowMultipleComponent]
+    public class BroTestingButton : MonoBehaviour
+    {
+        public const string ClassColor = "4EC9B0";
+        public const string StructColor = "86BB6F";
+        public const string MethodColor = "DCDCAA";
+        public const string None = "";
+
+        public const string Volume = "vol";
+        public const string Fade = "fade";
+        public const string AudioType = "type";
+        public const string ID = "id";
+        public const string Pitch = "pitch";
+        public const string Frequency = "frequency";
+        public const string TransitionParam = "transition";
+
+        public readonly string BaseFormat = "BroAudio".SetColor(ClassColor);
+        public readonly string MethodFormat = "." + "{0}".SetColor(MethodColor) + "({1})";
+        public readonly string EffectFormat = "Effect".SetColor(StructColor);
+
+        public const string Play = nameof(BroAudio.Play);
+        public const string SetVolume = nameof(BroAudio.SetVolume);
+        public const string SetPitch = nameof(BroAudio.SetPitch);
+        public const string SetEffect = nameof(BroAudio.SetEffect);
+        public const string Stop = nameof(BroAudio.Stop);
+
+        public const string LowPass = nameof(Effect.LowPass);
+        public const string ResetLowPass = nameof(Effect.ResetLowPass);
+        public const string HighPass = nameof(Effect.HighPass);
+        public const string ResetHighPass = nameof(Effect.ResetHighPass);
+
+        public const string Dominator = nameof(BroAudioChainingMethod.AsDominator);
+        public const string BGM = nameof(BroAudioChainingMethod.AsBGM);
+        public const string LowPasOthers = "LowPassOthers";
+        public const string HighPasOthers = "HighPasOthers";
+        public const string Transition = "Transition";
+
+        [SerializeField] bool _forceValidate = false;
+        [SerializeField] Button _button;
+        [SerializeField] Text _text;
+
+        private void OnValidate()
+        {
+            if (!_button || !_text)
+            {
+                return;
+            }
+            _text.text = string.Empty;
+            for (int i = 0; i < _button.onClick.GetPersistentEventCount(); i++)
+            {
+                if (i != 0)
+                {
+                    _text.text += "\n";
+                }
+                _text.text += GetAPIString(_button.onClick.GetPersistentMethodName(i));
+            }
+        }
+
+        private string GetAPIString(string methodName) => methodName switch
+        {
+            nameof(BroTesting.Play) => GetMainMethodText(Play),
+            nameof(BroTesting.SetVolume) => GetMainMethodText(SetVolume, Volume, Fade),
+            nameof(BroTesting.SetAudioTypeVolume) => GetMainMethodText(SetVolume, AudioType, Volume, Fade),
+            nameof(BroTesting.SetSoundIDVolume) => GetMainMethodText(SetVolume, ID, Volume, Fade),
+            nameof(BroTesting.PlayerSetVolume) => GetPlayerAppendMethodString(SetVolume, Volume, Fade),
+
+            nameof(BroTesting.SetPitch) => GetMainMethodText(SetPitch, Pitch, Fade),
+            nameof(BroTesting.SetAudioTypePitch) => GetMainMethodText(SetPitch, Pitch, AudioType, Fade),
+            nameof(BroTesting.PlayerSetPitch) => GetPlayerAppendMethodString(SetPitch, Pitch, Fade),
+
+            nameof(BroTesting.PlayerStop) => GetPlayerAppendMethodString(Stop, Fade),
+            nameof(BroTesting.StopSoundID) => GetMainMethodText(Stop, ID, Fade),
+            nameof(BroTesting.StopAudioType) => GetMainMethodText(Stop, AudioType, Fade),
+
+            nameof(BroTesting.SetLowPassFilter) => GetMainMethodText(SetEffect, GetEffectFactoryText(LowPass, Frequency, Fade), AudioType),
+            nameof(BroTesting.ResetLowPassFilter) => GetMainMethodText(SetEffect, GetEffectFactoryText(ResetLowPass, Fade), AudioType),
+            nameof(BroTesting.SetHighPassFilter) => GetMainMethodText(SetEffect, GetEffectFactoryText(HighPass, Frequency, Fade), AudioType),
+            nameof(BroTesting.ResetHighPassFilter) => GetMainMethodText(SetEffect, GetEffectFactoryText(ResetHighPass, Fade), AudioType),
+
+            nameof(BroTesting.PlayerAsLowPassDominator) => GetPlayerAppendMethodString(Dominator) + GetPlayerAppendMethodString(LowPasOthers, Frequency, Fade),
+            nameof(BroTesting.PlayerAsHighPassDominator) => GetPlayerAppendMethodString(Dominator) + GetPlayerAppendMethodString(HighPasOthers, Frequency, Fade),
+
+            nameof(BroTesting.PlayerAsBGM) => GetPlayerAppendMethodString(BGM) + GetPlayerAppendMethodString(Transition, TransitionParam, Fade),
+            _ => throw new System.NotImplementedException(),
+        };
+
+        private string GetMainMethodText(params string[] methodAndParameters)
+        {
+            return string.Format(BaseFormat + MethodFormat, GetMethodAndParameters(methodAndParameters));
+        }
+
+        private string GetEffectFactoryText(params string[] methodAndParameters)
+        {
+            return string.Format(EffectFormat + MethodFormat, GetMethodAndParameters(methodAndParameters));
+        }
+
+        private string[] GetMethodAndParameters(string[] parameters)
+        {
+            if (parameters.Length >= 1)
+            {
+                string[] result = new string[2];
+                result[0] = parameters[0];
+                result[1] = string.Empty;
+                for (int i = 1; i < parameters.Length; i++)
+                {
+                    string comma = i == parameters.Length - 1 ? string.Empty : ",";
+                    result[1] += parameters[i] + comma;
+                }
+                return result;
+            }
+            return null;
+        }
+
+        private string GetPlayerAppendMethodString(params string[] methodAndParameters)
+        {
+            return string.Format(MethodFormat, GetMethodAndParameters(methodAndParameters));
+        }
+    } 
+}

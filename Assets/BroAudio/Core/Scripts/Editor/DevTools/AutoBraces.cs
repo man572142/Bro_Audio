b9e8da17698c73dd044ed630cc33ee718f6ae10b
man572142 <man572142@gmail.com>
2024-08-10 17:36:35 +0800

    Add auto-generated code tool

---
+++ Assets/BroAudio/Core/Scripts/Editor/DevTools/AutoBraces.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System;
+
+namespace Ami.Extension.Reflection
+{
+    public struct AutoBraces : IDisposable
+    {
+        private StreamWriter _writer;
+        private string _indent;
+
+        public AutoBraces(StreamWriter writer, string text, ref string indent, string spaces = "    ")
+        {
+            _writer = writer;
+            _indent = indent;
+            if (string.IsNullOrEmpty(text))
+            {
+                _writer = null;
+                _indent = null;
+                return;
+            }
+
+            writer?.WriteLine(indent + text);
+            writer?.WriteLine(indent + "{");
+            indent += spaces;
+        }
+
+        public void Dispose()
+        {
+            _writer?.WriteLine(_indent + "}");
+        }
+    }
+
+    public static class AutoBracesFactory
+    {
+        public static AutoBraces WriteBraces(this StreamWriter writer, string text, ref string indent)
+        {
+            return new AutoBraces(writer, text, ref indent);
+        }
+    }
+}

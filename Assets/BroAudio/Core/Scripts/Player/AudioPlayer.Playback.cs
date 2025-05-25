979eaa45a631d216f7b839eab610255b05f1717a
man572142 <man572142@gmail.com>
2025-05-25 08:52:49 +0800

    Fixed exception thrown when calling Pause() a second time

--- Assets/BroAudio/Core/Scripts/Player/AudioPlayer.Playback.cs
+++ Assets/BroAudio/Core/Scripts/Player/AudioPlayer.Playback.cs
@@ -286,14 +286,14 @@ namespace Ami.BroAudio.Runtime
         void IAudioStoppable.Stop(float fadeOut, Action onFinished)
             => Stop(fadeOut, StopMode.Stop, onFinished);
         #endregion
-        public void Stop(float overrideFade, StopMode stopMode,Action onFinished)
+        public void Stop(float overrideFade, StopMode stopMode, Action onFinished)
         {
             if (IsStopping && overrideFade != Immediate)
             {
                 return;
             }
 
-            if (ID <= 0 || !AudioSource.isPlaying)
+            if (ID <= 0 || (!AudioSource.isPlaying && stopMode == StopMode.Stop))
             {
                 onFinished?.Invoke();
                 EndPlaying();

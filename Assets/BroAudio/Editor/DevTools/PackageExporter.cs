d099e6f7b07fc790d046f30653f63689dfc45072
Dan Whittaker <zallist@gmail.com>
2025-10-20 20:36:38 +0100

    Samples are now included when packaging. They are NOT stored in git, except in Samples~

--- Assets/BroAudio/Editor/DevTools/PackageExporter.cs
+++ Assets/BroAudio/Editor/DevTools/PackageExporter.cs
@@ -15,6 +15,16 @@ namespace Ami.BroAudio.Editor.DevTool
 		[MenuItem(BroName.MenuItem_BroAudio + "Export Package", priority = DevToolsMenuIndex + 13)]
         public static void Export()
         {
+            Copy("Assets/BroAudio/Samples~", "Assets/BroAudio/Samples", recursive: true);
+            Copy("Assets/BroAudio/Resources~", "Assets/BroAudio/Resources", recursive: false);
+            Copy("Assets/BroAudio/Resources~/Editor", "Assets/BroAudio/Editor/Resources", recursive: false);
+
+            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+
+            // Then wait for unity to update its db
+
+            // THEN export
+
 			List<string> allfilePaths = Directory.GetFiles("Assets/BroAudio", "*.*", SearchOption.AllDirectories)
                 .Select(x => x.Replace("\\", "/"))
                 .ToList();
@@ -28,6 +38,29 @@ namespace Ami.BroAudio.Editor.DevTool
             if(EditorUtility.DisplayDialog("Export BroAudio Package", $"Export Version:{BroAudioData.CodeBaseVersion} ?", "Yes", "No"))
             {
                 AssetDatabase.ExportPackage(allfilePaths.ToArray(), "BroAudio" + DateTime.Now.ToString("yy-MM-dd-HH-mm") + ".unitypackage", ExportPackageOptions.Interactive);
+            }
+        }
+
+        private static void Copy(string source, string target, bool recursive)
+        {
+            if (!Directory.Exists(target))
+            {
+                Directory.CreateDirectory(target);
+            }
+
+            foreach (string f in Directory.GetFiles(source, "*.*", SearchOption.TopDirectoryOnly))
+            {
+                string t = f.Replace(source, target);
+                File.Copy(f, t, true);
+            }
+
+            if (recursive)
+            {
+                foreach (string d in Directory.GetDirectories(source, "*", SearchOption.TopDirectoryOnly))
+                {
+                    string t = d.Replace(source, target);
+                    Copy(d, t, recursive);
+                }
             }
         }
 	}

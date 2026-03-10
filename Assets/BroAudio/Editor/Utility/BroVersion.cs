7a7de642ddf6d7c1347eb3cf33816dd46d1dbd05
man572142 <man572142@gmail.com>
2026-03-10 20:45:47 +0800

    Add temporary assembly reference to resolve compilation errors

--- Assets/BroAudio/Editor/Utility/BroVersion.cs
+++ Assets/BroAudio/Editor/Utility/BroVersion.cs
@@ -7,7 +7,7 @@ namespace Ami.BroAudio.Editor
 {
     public static class BroVersion
     {
-        public const string CodeBaseVersion = "3.1.0";
+        public const string CodeBaseVersion = "3.1.1";
 
         private const string VersionResourceName = "BroAudioVersion";
         public const string VersionFileName = VersionResourceName + ".txt";
